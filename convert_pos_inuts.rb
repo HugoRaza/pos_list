e9118cd1992f359b6f4e942b626b5889ce2a066c
Hugo Razafindranaly <hugo.raza@gmail.com>
2021-01-08 14:15:07 +0100

    test

--- convert_pos_inuts.rb
+++ convert_pos_inuts.rb
@@ -2,9 +2,13 @@ require "open-uri"
 require "csv"
 
 csv_path = URI.open("./pos_list.csv")
-
+array = []
 CSV.parse(csv_path, col_sep: ",", quote_char: '"', headers: :first_row) do |row|
   name = row["name"]
   code = row["code"]
-  puts "<option value=\"#{code}\">Agence #{name}</option>"
+
+  array << "<option value=\"#{code}\">Agence #{name.gsub("'", "\\'")}</option>\\n"
+
 end
+
+puts array.join
---
+++ select_pos_list_five.js
@@ -0,0 +1,5 @@
+let select_pos_name = document.getElementById("pos_name");
+let html_select = '<select id="pos_name" name="pos_name">\n<option value="12345">Agence d\'Yzeure</option>\n</select>'
+select_pos_name.innerHTML = html_select
+
+
